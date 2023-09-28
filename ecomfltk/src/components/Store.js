ea540fc70fb70df45f03b3b06f79036dc8cb6c09
Feraz Khan <ferazahmadgit62@gmail.com>
2023-09-28 13:10:19 +0530

    store js testing

--- ecomfltk/src/components/Store.js
+++ ecomfltk/src/components/Store.js
@@ -1,5 +1,32 @@
 import React from "react";
+import jeans1 from "../Images/jeans1.jpeg";
+import jeans2 from "../Images/jeans2.png";
+import "./store.css";
 
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <div className="grid-container">
+      <div className="grid-item">
+        <div>
+          <img src={jeans1} />
+        </div>
+        <div className="product__description">
+          <p className="product__brand">SUPERDRY</p>
+          <p className="product__style">Blue jeans</p>
+          <p className="product__rate"> 15000 </p>
+        </div>
+      </div>
+
+      <div className="grid-item">
+        <div>
+          <img src="https://s3.eu-north-1.amazonaws.com/feraz-product-image/images/prod0.jpg" />
+        </div>
+        <div className="product__description">
+          <p className="product__brand">SUPERDRY</p>
+          <p className="product__style">Blue jeans</p>
+          <p className="product__rate"> 15000 </p>
+        </div>
+      </div>
+    </div>
+  );
 };
